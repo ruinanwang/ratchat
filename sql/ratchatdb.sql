d1a6cefe26093842539c64d5e1a3e14b5562226e
Ruinan <nancywng@sina.com>
2018-03-06 10:47:32 -0500

    Update ratsite create table

--- sql/ratchatdb.sql
+++ sql/ratchatdb.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ratchatdb`.`ratsite` ( 
     `id` INT NOT NULL AUTO_INCREMENT , 
-    `time` DATETIME NOT NULL , 
+    `time` DATETIME CURRENT_TIMESTAMP , 
     `is_outside` BOOLEAN NOT NULL , 
     `is_alive` BOOLEAN NOT NULL , 
     `location` VARCHAR(255) NOT NULL , 
