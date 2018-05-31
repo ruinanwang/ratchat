a2ce9c907c4f5dcf3770d709592750f2f31022fd
Michael <michael@lawn-128-61-39-126.lawn.gatech.edu>
2018-05-31 16:56:36 -0400

    Adjusted SQL file to automatically create database schema along with tables and fixed some syntax inconsistencies.

---
+++ ratchat_db.sql
@@ -0,0 +1,35 @@
+
+/* DATABASE FILE
+  This file automatically creates the 'ratchatdb' 
+  database, along with its associated tables.
+  - Michael Koohang 
+*/
+
+CREATE DATABASE IF NOT EXISTS `ratchatdb`;
+USE `ratchatdb`;
+
+DROP TABLE IF EXISTS `ratevidence`;
+CREATE TABLE `ratevidence` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `droppings` tinyint(1) NOT NULL,
+  `chewed` tinyint(1) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zipcode` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ratsite`;
+CREATE TABLE `ratsite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_outside` tinyint(1) NOT NULL,
+  `is_alive` tinyint(1) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zipcode` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
