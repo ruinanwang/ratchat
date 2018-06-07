f1079114cf93f4eb67cb706a206d53734fc5213e
Michael <michael@lawn-128-61-39-126.lawn.gatech.edu>
2018-06-07 13:17:54 -0400

    Added reset when user makes 3 mistakes and edited prompts.

--- app/prompts.py
+++ app/prompts.py
@@ -5,69 +5,71 @@
 # Prompts for RatWatch survey.
 
 # General prompts.
-welcome = ('Welcome to RatWatch! Please reply with one of the following numbers:'
+welcome = ('Welcome to RatWatch! Type \'1\' or \'2\' or \'3\''
 + '\n  1. I saw a rat \n  2. I saw evidence of a rat'
-+ '\n  3. I want to prevent rats\nType \'1\' or \'2\' or \'3\'')
++ '\n  3. I want to prevent rats')
 
 welcome_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Please reply with one of the following numbers:'
++ 'Type \'1\' or \'2\' or \'3\''
 + '\n  1. I saw a rat \n  2. I saw evidence of a rat'
-+ '\n  3. I want to prevent rats \nType \'1\' or \'2\' or \'3\'')
++ '\n  3. I want to prevent rats \n')
 
 city = ('Please type the city. For example \'Atlanta\'')
 
 city_error = ('Your input was incorrect. \nPlease try again. \n\n'
 + 'Please type the city. For example \'Atlanta\'')
 
-zipcode = ('Please type the zipcode. For example \'30332\'')
+zipcode = ('Please type the ZIP code. For example \'30332\'')
 
 zipcode_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Please type the zipcode. For example \'30332\'')
++ 'Please type the ZIP code. For example \'30332\'')
 
-survey_complete = ('You have completed the survey. Thank you!')
+survey_complete = ('You have completed the survey. Thank you for your response! Just text us again if you want to make another report :)')
 
-survey_complete_image = ('Thanks for the image! You have completed the survey.')
+mistakes_prompt = ('You have made too many errors on the survey. To restart, please text us again.')
+
+restart = ('If you would like to restart the survey, just type \'RESTART\'')
 
 # Rat siting prompts.
-site_address = ('Where did you see the rat?\n\nType the street name and house number. For example \'120 Main Street\' '
+site_address = ('Where did you see the rat?\n\nType the house number and street name. For example \'120 Main Street\' '
 + '\n\nIf you don\'t know the house number, you can just type the street name. For example \'Main Street\'')
 
 site_address_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Where did you see the rat?\nType the street name and house number. For example \'120 Main Street\' '
++ 'Where did you see the rat?\nType the house number and street name. For example \'120 Main Street\' '
 + '\n\nIf you don\'t know the house number, you can just type the street name. For example \'Main Street\'')
 
-in_out = ('Where did you see the rat? \n 1. Inside \n 2. Outside \nType \'1\' or \'2\'')
+in_out = ('Was the rat inside or outside?\nType \'1\' or \'2\' \n 1. Inside \n 2. Outside ')
 
 in_out_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Where did you see the rat? \n 1. Inside \n 2.Outside \nType \'1\' or \'2\'')
++ 'Was the rat inside or outside?\nType \'1\' or \'2\' \n 1. Inside \n 2. Outside ')
 
-dead_or_alive = ('Was the rat dead or alive? \n 1. Dead \n 2. Alive \nType \'1\' or \'2\'')
+dead_or_alive = ('Was the rat dead or alive?\nType \'1\' or \'2\' \n 1. Dead \n 2. Alive ')
 
 dead_or_alive_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Was the rat dead or alive? \n 1. Dead \n 2. Alive \nType \'1\' or \'2\'')
++ 'Was the rat dead or alive?\nType \'1\' or \'2\' \n 1. Dead \n 2. Alive ')
 
-site_picture = ('Please send us a picture of the rat or where your saw the rat. Otherwise, type \'DONE\' to finish the survey.')
+site_picture = ('Please send us a picture of the rat or where your saw the rat. Or, you can type \'DONE\' to finish the survey.')
 
 site_picture_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Please send us a picture of the rat or where your saw the rat.\nOtherwise, type \'DONE\' to finish the survey.')
++ 'Please send us a picture of the rat or where your saw the rat. Or, you can type \'DONE\' to finish the survey.')
 
 # Rat evidence prompts.
-evidence_address = ('Where did you see the evidence?\n\nType the street name and house number. For example \'120 Main Street\' '
+evidence_address = ('Where did you see the evidence?\n\nType the house number and street name. For example \'120 Main Street\' '
 + '\n\nIf you don\'t know the house number, you can just type the street name. For example \'Main Street\'')
 
 evidence_address_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Where did you see the evidence?\n\nType the street name and house number. For example \'120 Main Street\' '
++ 'Where did you see the evidence?\n\nType the house number and street name. For example \'120 Main Street\' '
 + '\n\nIf you don\'t know the house number, you can just type the street name. For example \'Main Street\'')
 
-category = ('Please categorize your evidence:\n 1. Rat droppings\n 2. Chewed boxes or food \nType \'1\' or \'2\'')
+category = ('What kind of evidence did you find?\nType \'1\' or \'2\'\n 1. Rat droppings\n 2. Chewed boxes or food ')
 
 category_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Please categorize your evidence:\n 1. Rat droppings\n 2. Chewed boxes or food \nType \'1\' or \'2\'')
++ 'What kind of evidence did you find?\nType \'1\' or \'2\'\n 1. Rat droppings\n 2. Chewed boxes or food')
 
-evidence_picture = ('Please send us a picture of the evidence. Otherwise, type \'DONE\' to finish the survey.')
+evidence_picture = ('Please send us a picture of the evidence. Or, you can type \'DONE\' to finish the survey.')
 
 evidence_picture_error = ('Your input was incorrect. \nPlease try again. \n\n'
-+ 'Please send us a picture of the evidence. Otherwise, type \'DONE\' to end the survey.')
++ 'Please send us a picture of the evidence. Or, you can type \'DONE\' to end the survey.')
 
 # Interest prompts.
 prevention = ('Thank you for your interest in rat prevention. Please follow this link for more info:\nlinkhere')
--- app/ratwatch.py
+++ app/ratwatch.py
@@ -36,7 +36,7 @@ city_error = prompts.city_error
 zipcode = prompts.zipcode
 zipcode_error = prompts.zipcode_error
 survey_complete = prompts.survey_complete
-survey_complete_image = prompts.survey_complete_image
+mistakes_prompt = prompts.mistakes_prompt
 
 # Rat siting prompts.
 site_address = prompts.site_address
@@ -56,11 +56,13 @@ category_error = prompts.category_error
 evidence_picture = prompts.evidence_picture
 evidence_picture_error = prompts.evidence_picture_error
 
-# SQL statements for inserting and updating information.
-addSiteSQL = config.addSiteSQL
-addEvidenceSQL = config.addEvidenceSQL
-updateSiteImageSQL = config.updateSiteImageSQL
-updateEvidenceImageSQL = config.updateEvidenceImageSQL
+# SQL statements for inserting, deleting, and updating information.
+add_site_sql = config.add_site_sql
+add_evidence_sql = config.add_evidence_sql
+delete_site_sql = config.delete_site_sql
+delete_evidence_sql = config.delete_evidence_sql
+update_site_image_sql = config.update_site_image_sql
+update_evidence_image_sql = config.update_evidence_image_sql
 
 # Function that provides instructions
 # to Twilio on how to respond to an
@@ -73,12 +75,12 @@ def process_message():
     userInput = request.values.get('Body', None)
     counter = session.get('counter', 0)
     case = session.get('case', 0)
+    mistakes = session.get('mistakes', 0)
 
-    # Case 0 - Start
+    # Start of survey.
     if (counter == 0):
         message.body(welcome)
-        counter += 1
-        session['counter'] = counter
+        session['counter'] = counter + 1
         response.append(message)
         return str(response)
 
@@ -96,6 +98,13 @@ def process_message():
             session['case'] = 3
             case = session.get('case', 0)
         else:
+            session['mistakes'] = mistakes + 1
+            mistakes = session['mistakes']
+            if (mistakes == 3):
+                session.clear()
+                message.body(mistakes_prompt)
+                response.append(message)
+                return str(response)
             message.body(welcome_error)
 
     # Decision tree that carries out the appropriate 
@@ -103,44 +112,71 @@ def process_message():
     if (case == 1):
         if (counter == 1):
             message.body(site_address)
-            counter += 1         
-            session['counter'] = counter
+            session['counter'] = counter + 1
         elif (counter == 2):
             if (userInput.replace(' ', '').isalnum()):
                 session['site_street'] = userInput
                 message.body(city)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(site_address_error)
         elif (counter == 3):
             if (userInput.isalpha()):
                 session['site_city'] = userInput
                 message.body(zipcode)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0                
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(city_error)
         elif (counter == 4):
             if (userInput.isdigit() and len(userInput) == 5):
                 session['site_zipcode'] = userInput
                 message.body(in_out)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(zipcode_error)
         elif (counter == 5):
             if userInput == '1':
                 session['site_is_outside'] = False
                 message.body(dead_or_alive)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             elif userInput == '2':
                 session['site_is_outside'] = True
                 message.body(dead_or_alive)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(in_out_error)
         elif (counter == 6):
             if userInput == '1':
@@ -151,11 +187,11 @@ def process_message():
                 site_city = session.get('site_city', 0)
                 site_zipcode = session.get('site_zipcode', 0)
 
-                cursor.execute(addSiteSQL,(site_is_outside, site_is_alive, site_street, site_city, site_zipcode))
+                cursor.execute(add_site_sql,(site_is_outside, site_is_alive, site_street, site_city, site_zipcode))
                 session['rowId'] = cursor.lastrowid
                 connection.commit()
-                counter += 1
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
                 message.body(site_picture)
             elif userInput == '2':
                 session['site_is_alive'] = True
@@ -165,59 +201,95 @@ def process_message():
                 site_city = session.get('site_city', 0)
                 site_zipcode = session.get('site_zipcode', 0)
 
-                cursor.execute(addSiteSQL,(site_is_outside, site_is_alive, site_street, site_city, site_zipcode))
+                cursor.execute(add_site_sql,(site_is_outside, site_is_alive, site_street, site_city, site_zipcode))
                 session['rowId'] = cursor.lastrowid
                 connection.commit()
-                counter += 1
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
                 message.body(site_picture)
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(dead_or_alive_error)
         elif (counter == 7):
             if request.values['NumMedia'] != '0':
-                filename = request.values['MessageSid'] + '.png'
+                filename = request.values['MessageSid'] + '.jpg'
                 filepath = IMAGE_DOWNLOAD_DIRECTORY + filename
                 with open(filepath, 'wb') as f:
                     image_url = request.values['MediaUrl0']
                     f.write(requests.get(image_url).content)
-                cursor.execute(updateSiteImageSQL, (filepath, session['rowId']))
+                cursor.execute(update_site_image_sql, (filepath, session['rowId']))
                 connection.commit()
                 session.clear()
-                message.body(survey_complete_image)
-            elif (userInput.upper() == 'DONE'):
+                message.body(survey_complete)
+            elif (userInput.lower() == 'DONE'):
                 session.clear()
                 message.body(survey_complete)   
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    cursor.execute(delete_site_sql, (session['rowId'],))
+                    connection.commit()
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(site_picture_error)
 
     elif (case == 2):
         if (counter == 1):
             message.body(evidence_address)
-            counter += 1         
-            session['counter'] = counter
+            session['counter'] = counter + 1
         elif (counter == 2):
             if (userInput.replace(' ', '').isalnum()):
                 session['evidence_street'] = userInput
                 message.body(city)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(evidence_address_error)
         elif (counter == 3):
             if (userInput.isalpha()):
                 session['evidence_city'] = userInput
                 message.body(zipcode)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(city_error)
         elif (counter == 4):
             if (userInput.isdigit() and len(userInput) == 5):
                 session['evidence_zipcode'] = userInput
                 message.body(category)
-                counter += 1         
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(zipcode_error)
         elif (counter == 5):
             if userInput == '1':
@@ -230,11 +302,11 @@ def process_message():
                 evidence_city = session.get('evidence_city', 0)
                 evidence_zipcode = session.get('evidence_zipcode', 0)
 
-                cursor.execute(addEvidenceSQL,(evidence_droppings, evidence_chewed, evidence_street, evidence_city, evidence_zipcode))                
+                cursor.execute(add_evidence_sql,(evidence_droppings, evidence_chewed, evidence_street, evidence_city, evidence_zipcode))                
                 session['rowId'] = cursor.lastrowid
                 connection.commit()
-                counter += 1
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
                 message.body(evidence_picture)
             elif userInput == '2':
                 session['evidence_droppings'] = False
@@ -246,30 +318,46 @@ def process_message():
                 evidence_city = session.get('evidence_city', 0)
                 evidence_zipcode = session.get('evidence_zipcode', 0)
 
-                cursor.execute(addEvidenceSQL,(evidence_droppings, evidence_chewed, evidence_street, evidence_city, evidence_zipcode))
+                cursor.execute(add_evidence_sql,(evidence_droppings, evidence_chewed, evidence_street, evidence_city, evidence_zipcode))
                 session['rowId'] = cursor.lastrowid
                 connection.commit()
-                counter += 1
-                session['counter'] = counter
+                session['counter'] = counter + 1
+                session['mistakes'] = 0
                 message.body(evidence_picture)
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(category_error)
 
         elif (counter == 6):
             if request.values['NumMedia'] != '0':
-                filename = request.values['MessageSid'] + '.png'
+                filename = request.values['MessageSid'] + '.jpg'
                 filepath = IMAGE_DOWNLOAD_DIRECTORY + filename
                 with open(filepath, 'wb') as f:
                     image_url = request.values['MediaUrl0']
                     f.write(requests.get(image_url).content)
-                cursor.execute(updateEvidenceImageSQL, (filepath, session['rowId']))
+                cursor.execute(update_evidence_image_sql, (filepath, session['rowId']))
                 connection.commit()
                 session.clear()
-                message.body(survey_complete_image)
-            elif (userInput.upper() == 'DONE'):
+                message.body(survey_complete)
+            elif (userInput.lower() == 'DONE'):
                 session.clear()
                 message.body(survey_complete)   
             else:
+                session['mistakes'] = mistakes + 1
+                mistakes = session['mistakes']
+                if (mistakes == 3):
+                    cursor.execute(delete_evidence_sql, (session['rowId'],))
+                    connection.commit()
+                    session.clear()
+                    message.body(mistakes_prompt)
+                    response.append(message)
+                    return str(response)
                 message.body(evidence_picture_error)
 
     elif (case == 3):
